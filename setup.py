7c81444013d3a818ee0e07d6f5df3a408abd946a
panni <panni@fragstore.net>
2018-02-21 13:31:19 +0100

    Revert "remove py3"
    This reverts commit dd787c6

--- setup.py
+++ setup.py
@@ -113,4 +113,5 @@ setuptools.setup(
         'Operating System :: Microsoft :: Windows :: Windows NT/2000',
         'Operating System :: POSIX',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',])
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'])
